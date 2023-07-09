dec043e87f95448351af324edecf4a3bb762b7f0
JosenRomero <romerojosemanuel@hotmail.com>
2023-07-09 17:59:07 -0600

    add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ handleButton, text }) => {
+  return (
+    <button
+      onClick={handleButton}
+      name={text}
+      className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2'
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button
