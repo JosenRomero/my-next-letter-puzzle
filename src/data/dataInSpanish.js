66e032bb37cc125b78825b9c92a449d1ca274e08
JosenRomero <romerojosemanuel@hotmail.com>
2023-07-11 20:00:52 -0600

    add data folder

---
+++ src/data/dataInSpanish.js
@@ -0,0 +1,7 @@
+export const dataInSpanish = [
+  {
+    topic: "dormitorío",
+    letters: ["c", "a", "m", "p", "u", "e", "r", "t", "l", "z"],
+    answer: ["cama", "puerta", "luz"],
+  },
+]
---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+import { dataInSpanish } from "./dataInSpanish.js"
+
+export const data = {
+  es: dataInSpanish,
+  en: [],
+}
