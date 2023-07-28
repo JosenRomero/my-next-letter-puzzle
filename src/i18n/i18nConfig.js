b9e5c0dfb547077bd6d549185559e7576a728cbb
JosenRomero <romerojosemanuel@hotmail.com>
2023-07-27 20:28:41 -0600

    add i18next config

---
+++ src/i18n/i18nConfig.js
@@ -0,0 +1,26 @@
+import i18n from "i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
+import { initReactI18next } from "react-i18next"
+import en from "./locales/en/translations.json"
+import es from "./locales/es/translations.json"
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    supportedLngs: ["en", "es"],
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation: en,
+      },
+      es: {
+        translation: es,
+      },
+    },
+  })
+
+export default i18n
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import { UserProvider } from "./context/UserContext.jsx"
 import "./index.css"
+import "./i18n/i18nConfig.js"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
