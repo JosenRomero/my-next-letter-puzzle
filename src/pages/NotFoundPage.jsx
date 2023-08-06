be16bedcb265e3cbb397a5343f01c611b70ab02d
JosenRomero <romerojosemanuel@hotmail.com>
2023-08-05 20:45:31 -0600

    update NotFoundPage.jsx

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,7 +3,11 @@ import { useTranslation } from "react-i18next"
 const NotFoundPage = () => {
   const { t } = useTranslation()
 
-  return <div>{t("title.notFoundPage")}</div>
+  return (
+    <div className='flex items-center justify-center'>
+      {t("title.notFoundPage")}
+    </div>
+  )
 }
 
 export default NotFoundPage
