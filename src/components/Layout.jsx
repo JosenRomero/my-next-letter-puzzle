0c23a0c52869d81afabda9f00aeb4904d702ea52
JosenRomero <romerojosemanuel@hotmail.com>
2023-08-05 19:39:19 -0600

    change font family

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Footer from "./Footer"
 
 const Layout = ({ children }) => {
   return (
-    <div className='min-h-screen grid grid-cols-1 place-content-between bg-white dark:bg-zinc-800'>
+    <div className='min-h-screen grid grid-cols-1 place-content-between bg-white dark:bg-zinc-800 font-mono'>
       <Menu />
       <main className='lg:w-1/3 mx-auto min-h-[70vh] rounded-xl'>
         <div className='h-full p-8 text-zinc-800 dark:text-white grid grid-cols-1 place-content-center'>
