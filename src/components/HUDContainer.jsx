0a2c0efae1e10b91642711a433cfd9e3b9bea414
JosenRomero <romerojosemanuel@hotmail.com>
2023-08-10 21:07:23 -0600

    update HUDContainer coomponent

--- src/components/HUDContainer.jsx
+++ src/components/HUDContainer.jsx
@@ -4,16 +4,20 @@ const HUDContainer = ({ level, wordsLength, wordsLengthTotal, topic }) => {
   const { t } = useTranslation()
 
   return (
-    <div className='text-gray-900 mb-5 py-2 border-b dark:text-gray-400 dark:border-gray-600'>
-      <div className='flex justify-between'>
-        <p>
-          {t("HUD.level")}: {level}
-        </p>
-        <p>{topic}</p>
-        <p>
-          <span>{t("HUD.word")}: </span>
-          {wordsLengthTotal != 0 ? `${wordsLength}/${wordsLengthTotal}` : ""}
-        </p>
+    <div className='text-gray-900 mb-5 dark:text-gray-400'>
+      <div className='flex flex-col gap-3'>
+        <div className='flex justify-between pb-2 border-b dark:border-gray-600'>
+          <p>
+            {t("HUD.level")}: {level}
+          </p>
+          <p>
+            <span>{t("HUD.word")}: </span>
+            {wordsLengthTotal != 0 ? `${wordsLength}/${wordsLengthTotal}` : ""}
+          </p>
+        </div>
+        <div className='flex justify-center pb-2 border-b dark:border-gray-600'>
+          <p>{topic}</p>
+        </div>
       </div>
     </div>
   )
