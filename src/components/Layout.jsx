4473836e1c24d23fd493a53d9483ef9773cf6010
Jose <romerojosemanuel@hotmail.com>
2023-09-04 17:45:00 -0600

    update Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     <div className='min-h-screen grid grid-cols-1 place-content-between bg-gray-200 dark:bg-slate-900 font-mono'>
       <Menu />
       <main className='w-full mx-auto min-h-[70vh] rounded-xl'>
-        <div className='h-full p-8 text-zinc-800 dark:text-white grid grid-cols-1 place-content-center'>
+        <div className='h-full p-2 sm:p-8 text-zinc-800 dark:text-white grid grid-cols-1 place-content-center'>
           {children}
         </div>
       </main>
