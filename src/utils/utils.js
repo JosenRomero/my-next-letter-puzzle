2879fc8936c076880f7309c5d4ceb22ef31734dc
Jose <romerojosemanuel@hotmail.com>
2023-09-14 18:59:59 -0600

    fix: duplicate words

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,14 +4,16 @@ export const checkWords = (level, language, words) => {
   let { answer, valid_answer } = data[language][level]
   let arr = []
   let nextLevel = true
+  let rightAnswers = []
 
   for (let i = 0; i < answer.length; i++) {
-    if (answer.includes(words[i]) || valid_answer.includes(words[i])) {
-      arr.push("o")
-    } else {
-      arr.push("x")
-      nextLevel = false
-    }
+    let result =
+      (answer.includes(words[i]) || valid_answer.includes(words[i])) &&
+      !rightAnswers.includes(words[i])
+
+    arr.push(`${result ? "o" : "x"}`)
+    if (result) rightAnswers.push(words[i])
+    else nextLevel = false
   }
 
   return { nextLevel, arr }
