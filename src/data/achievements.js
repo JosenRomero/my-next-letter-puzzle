779055a5fe4429187b160502987d47a02aca467f
Jose <romerojosemanuel@hotmail.com>
2024-02-25 18:03:03 -0600

    add react-toastify

--- src/data/achievements.js
+++ src/data/achievements.js
@@ -2,6 +2,12 @@ import achievement_easy from "../images/achievement_easy.svg"
 import achievement_normal from "../images/achievement_normal.svg"
 import achievement_hard from "../images/achievement_hard.svg"
 
+export const achievementUnlocked = {
+  "10": "Logro desbloqueado: Fácil",
+  "20": "Logro desbloqueado: Normal",
+  "25": "Logro desbloqueado: Difícil"
+}
+
 export const achievements = [
   {
     img: achievement_easy,
--- src/pages/PlayPage.jsx
+++ src/pages/PlayPage.jsx
@@ -1,7 +1,10 @@
 import { useState, useContext } from "react"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 import BoxContainer from "../components/BoxContainer.jsx"
 import ResultContainer from "../components/ResultContainer.jsx"
 import { UserContext } from "../context/UserContext.jsx"
+import { achievementUnlocked } from "../data/achievements.js"
 
 const PlayPage = () => {
   const { level, updateLevel } = useContext(UserContext)
@@ -10,7 +13,17 @@ const PlayPage = () => {
 
   const updateWin = (value) => {
     setWin(value)
-    if (value?.nextLevel) updateLevel()
+
+    if (value?.nextLevel) {
+
+      if(achievementUnlocked[level]) {
+        toastSuccess(`${achievementUnlocked[level]}`)
+      }
+      
+      updateLevel()
+
+    }
+
   }
 
   const nextLevel = () => {
@@ -19,8 +32,14 @@ const PlayPage = () => {
 
   const tryAgain = () => updateWin(null)
 
+  const toastSuccess = (text) => toast.success(text)
+
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        pauseOnHover={false} 
+      />
       {win === null ? (
         <BoxContainer level={level} updateWin={updateWin} />
       ) : (
