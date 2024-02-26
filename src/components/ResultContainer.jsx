4612d937f1ffae4a6e9d31862b15e8046846cf34
Jose <romerojosemanuel@hotmail.com>
2024-02-26 16:50:28 -0600

    add canvas-confetti

--- src/components/ResultContainer.jsx
+++ src/components/ResultContainer.jsx
@@ -1,10 +1,13 @@
 import { useTranslation } from "react-i18next"
+import confetti from "canvas-confetti"
 import Button from "./Button"
 import CirclesContainer from "./CirclesContainer"
 import { TOTAL_LEVELS } from "../utils/CONSTANTS"
 
 const ResultContainer = ({ win, level, nextLevel, tryAgain }) => {
   const { t } = useTranslation()
+
+  if(win.nextLevel && level > TOTAL_LEVELS) confetti()
 
   return (
     <div className='bg-white lg:w-1/3 mx-auto flex flex-col gap-5 p-8 rounded-lg dark:bg-slate-800 border border-gray-200 shadow dark:border-gray-700 animate-fade-in-down'>
@@ -13,7 +16,7 @@ const ResultContainer = ({ win, level, nextLevel, tryAgain }) => {
       </h3>
       <CirclesContainer arr={win.arr} />
       {win.nextLevel && level > TOTAL_LEVELS && (
-        <p className='p-2 bg-lime-500 text-white text-center'>{t("endText")}</p>
+        <p className='p-2 bg-green-600 text-green-100 text-center mt-5'>{t("endText")}</p>
       )}
       <div className="text-center">
         {win.nextLevel && level <= TOTAL_LEVELS && (
