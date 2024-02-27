c9f08e489e1660011c73efc5119eeb969e923f64
Jose <romerojosemanuel@hotmail.com>
2024-02-26 18:27:13 -0600

    update Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ handleButton, text, css = "bg-cyan-500 hover:bg-cyan-600 dark:
       onClick={handleButton}
       name={text}
       className={`focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ${
-        disabled ? "cursor-not-allowed" : "" } ${css} `}
+        disabled ? "cursor-not-allowed !bg-gray-300 !hover:bg-gray-300 dark:!bg-gray-600 dark:!hover:bg-gray-600" : "" } ${css} `}
       disabled={disabled}
     >
       {text}
--- src/components/DangerAlert.jsx
+++ src/components/DangerAlert.jsx
@@ -5,7 +5,7 @@ const DangerAlert = ({ show, text }) => {
   if(!show) return <></>
 
   return (
-    <div className="flex gap-2 items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+    <div className="flex gap-2 items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-900 dark:text-red-400" role="alert">
       <FaCircleInfo size={16} />
       <div>{text}</div>
     </div>
