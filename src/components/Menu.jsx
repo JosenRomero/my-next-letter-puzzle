e617cfe2fe2ea800a853f8992d79dfbc02805eac
Jose <romerojosemanuel@hotmail.com>
2024-03-02 18:39:39 -0600

    update Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import ChangeTheme from "./ChangeTheme"
 import HowToPlay from "./HowToPlay"
 import icon from "../images/icon.svg"
@@ -9,14 +9,19 @@ const Menu = () => {
       <div className='md:w-3/4 mx-auto flex items-center md:justify-between gap-x-3'>
         <div className='flex gap-2 items-center'>
           <img className='w-16 h-16' src={icon} />
-          <Link
-            className='text-cyan-500 hover:underline hover:text-cyan-500 dark:text-white font-bold text-lg'
-            to={"/"}
-          >
-            Next Letter Puzzle
-          </Link>
         </div>
         <div className='text-zinc-800 dark:text-white flex gap-x-5'>
+          <NavLink
+            className={({ isActive }) => 
+              [
+                isActive ? "border-b-2 border-cyan-500" : "",
+                "font-bold text-lg text-cyan-500 hover:text-cyan-500 hover:opacity-60"
+              ].join(" ")
+            }
+            to={"/"}
+          >
+            Inicio
+          </NavLink>
           <HowToPlay />
           <ChangeTheme />
         </div>
